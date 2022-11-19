e75eee241836086134177cbcf5003e50837c018c
ThinkPad <Tonan041@gmail.com>
2022-11-19 17:29:48 +0100

    123

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задание №1
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        //Задание №2
+        int clientOS0 = 1;
+        int clientDeviceYear = 2015;
+        if (clientOS0 == 0) {
+            if (clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        } else if (clientOS0 == 1) {
+            if (clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        }
+        //Задание №3
+        int year = 300;
+        if (year % 100 == 0 || year % 4 == 0 && year % 400 != 0) {
+            System.out.println("год является високосным");
+        }
+        else System.out.println("год не является високосным");
+    }
+}
